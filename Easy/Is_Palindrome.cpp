f030e09ecc3ad23a681ae60cc14d77e462ce5bdf
Mahfuz001 <likhon.codew@gmail.com>
2019-08-11 09:37:15 +0600

    solved Is Palindrome

---
+++ Easy/Is_Palindrome.cpp
@@ -0,0 +1,34 @@
+/**
+ * 	author: MahfuzAhmed
+ * 	created: 11.08.2019 
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x) cout << '>' << #x << ':' << x << endl;
+//~ const double pi = 2*acos(0.0);
+//~ const long long mod = 1e9+7;
+//~ const int MAX_N = 1e6+5;
+
+int main(int argc, char* argv[]){
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+       
+    #ifdef _DEBUG
+    freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    #endif
+    
+    string s; cin >> s;
+    string x = s;
+    reverse(x.begin(), x.end());
+    
+    if(s == x){
+        cout << "Yes\n";
+    }else{
+        cout << "No\n";
+    }
+    
+    
+    return 0;
+}
