79863523d85f55508850353db2472604ea23dd24
Mahfuz001 <likhon.codew@gmail.com>
2019-08-11 09:47:37 +0600

    solved Making Friends

---
+++ Easy/Making_Friends.cpp
@@ -0,0 +1,33 @@
+/**
+ * 	author: MahfuzAhmed
+ * 	created: 11.08.2019 
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x) cout << '>' << #x << ':' << x << endl;
+//~ const double pi = 2*acos(0.0);
+//~ const long long mod = 1e9+7;
+//~ const int MAX_N = 1e6+5;
+
+int main(int argc, char* argv[]){
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+       
+    #ifdef _DEBUG
+    freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    #endif
+    
+    int n; cin >> n;
+    
+    int cnt = 0;
+    
+    for(int i = 1; i < n; i++){
+        if(n % i == 0) cnt++;
+    }
+    
+    cout << cnt << "\n";
+    
+    return 0;
+}
